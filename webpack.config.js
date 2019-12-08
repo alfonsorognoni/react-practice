6b628391510bad827cce238d14b6100c64912531
Alfonso Rognoni <alfonsorognoni@gmail.com>
2019-12-08 02:07:45 -0300

    adding sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -15,15 +15,29 @@ module.exports = {
             {
                 test: /\.(js||jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
             },
             {
                 test: /\.html$/,
-                use: {
-                    loader: "html-loader"
-                }
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -31,6 +45,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
         })
     ]
 }

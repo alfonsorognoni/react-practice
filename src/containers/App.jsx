8a4f33633670ba569ac8b4cba5b5b7dd24e8883e
Alfonso 7l <alfonso@7lfreight.com>
2019-12-16 14:43:14 -0300

    hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,47 +6,54 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
+import useInitialState from '../hooks/useInitialState';
+
 import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState';
 
 const App = () => {
 
-  const [videos, setVideos] = useState([]);
+  // const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/initalState')
-      .then(response => response.json())
-      .then(data => setVideos(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/initalState')
+  //     .then(response => response.json())
+  //     .then(data => setVideos(data));
+  // }, []);
 
-  console.log(videos);
+  const InitialState = useInitialState(API);
 
   return (
     <div className='App'>
       <Header />
       <Search />
-      <Categories title='Mi Lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {InitialState.mylist && InitialState.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            {InitialState.mylist && InitialState.mylist.map((item) => (
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
 
       <Categories title='Tendencias'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {InitialState.trends && InitialState.trends.map((item) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales'>
+        <Carousel>
+          {InitialState.originals && InitialState.originals.map((item) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
       <Footer />
